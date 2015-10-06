773c3c757ed540ec61b082844703457a1be068d9
Dan Rusk <djrusk@gmail.com>
2015-10-06 18:48:21 -0400

    Add Question1Test and beginning of Question2Test.  Make sure to do Maven update project command.

--- src/test/java/edu/carrollcc/cis232/Question2Test.java
+++ src/test/java/edu/carrollcc/cis232/Question2Test.java
@@ -1,5 +1,20 @@
 package edu.carrollcc.cis232;
 
-public class Question2Test {
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.io.Serializable;
+
+import org.junit.Test;
+
+public class Question2Test {
+	@Test
+	public void testTestScoresImplementsSerializable() {
+		try {
+			Class<?> scores = Class.forName("edu.carrollcc.cis232.TestScores");
+			assertTrue("TestScores must implement Serializable", Serializable.class.isAssignableFrom(scores));
+		} catch (ClassNotFoundException e) {
+			fail("TestScores does not exist!");
+		}
+	}
 }
